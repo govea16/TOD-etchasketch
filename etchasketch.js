9954b78ca18f8481d5bee3fca209838baff900a5
govea16 <49473134+govea16@users.noreply.github.com>
2025-04-08 23:58:21 -0600

    finished Odin Project requirements
    Working on completing the extra credit parts.

---
+++ etchasketch.js
@@ -0,0 +1,49 @@
+const mainContent = document.querySelector('.main-content');
+mainContent.style.display = 'flex';
+mainContent.style.flexDirection = 'column';
+mainContent.style.alignItems = 'center';
+
+
+const numberOfSquares = document.createElement('button');
+const btnDiv = document.querySelector('.grid-button');
+btnDiv.appendChild(numberOfSquares);
+
+numberOfSquares.textContent = 'Click to customize size'
+let numOfSquares = 16;
+numberOfSquares.addEventListener("click", () => {
+    let numOfSquares = prompt("Please enter a number to 100 max to customize how many squares for one side of the grid:");
+    if (numOfSquares > 100) {
+        return;
+    }
+    gridCreate(numOfSquares);
+});
+
+function increaseOpacity () {
+
+}
+
+
+function gridCreate (gridSize) {
+    mainContent.innerHTML = "";
+    for (let i = 1; i <= gridSize; i++) {
+        const gameRow = document.createElement('div');
+        gameRow.style.display = 'flex';
+        gameRow.style.flexDirection = 'row';
+        gameRow.style.height = `${800/gridSize}px`;
+        mainContent.appendChild(gameRow);
+        for (let j = 1; j <= gridSize; j++) {
+            const gameSquare = document.createElement('div')
+            gameSquare.addEventListener('mouseenter', () => {
+                gameSquare.style.backgroundColor = 'black';
+            });
+            gameSquare.style.width = `${800/gridSize}px`;
+            gameSquare.style.border = '.5px solid #000000';
+            gameSquare.style.flex = "50 50 50";
+            gameRow.appendChild(gameSquare);
+    
+        } 
+    }
+    
+}
+
+gridCreate(numOfSquares);
